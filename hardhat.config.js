c48010561acc28acf84236c76876d0849f9e21a1
monickark <monicka.rk@gmail.com>
2026-05-14 16:44:27 +0530

    folder structure rearrange 1  amber

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,20 +1,22 @@
-var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic = "rely viable waste novel ice festival wrap round effort improve endless strong";
-module.exports = {
- networks: {
-  development: {
-   host: "127.0.0.1",
-   port: 8545,
-   network_id: "*"
-  },
-  rinkeby: {
-      provider: function() { 
-       return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/015bf16116de4e21af477e6e44f3d206");
-      },
-      network_id: 4,
-      gas: 4500000,
-      gasPrice: 10000000000,
-  }
- }
-};
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 
+module.exports = {
+  solidity: "0.8.20",
+
+  networks: {
+    hardhat: {},
+
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+
+    amoy: {
+      url: process.env.RPC_URL,
+      accounts:
+        process.env.PRIVATE_KEY
+          ? [process.env.PRIVATE_KEY]
+          : []
+    }
+  }
+};
